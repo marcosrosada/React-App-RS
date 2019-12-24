c4f1e054a6be5c2cb428b7b28cc3f79b594c1ec4
Marcos Rosada <ochild@gmail.com>
2019-12-24 12:06:59 -0300

    Render results

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -22,7 +22,7 @@ export default class Main extends Component {
         return (
             <div className="product-list">
                 {this.state.products.map(product => (
-                    <h2>{ product.title }</h2>
+                    <h2 key={product.id}>{ product.title }</h2>
                 ))}
             </div>
         );
